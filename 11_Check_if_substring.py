7d59834a31f8c8b0a7b69f68f0ad7f103962531d
YuriyKlokov <60291552+YuriyKlokov@users.noreply.github.com>
2022-11-02 02:29:27 +0300

    Add files via upload

--- 11_Check_if_substring.py
+++ 11_Check_if_substring.py
@@ -1,19 +1,20 @@
 def check_if_substring(substring: str, word: str) -> bool:
-    """
-    This function check if word contains substring
-    
-    @substring: str
-    @word: str
-    @return: bool
-    examples: substring = 'abc'
-              word  = 'ahbgdc'
-              check_if_substring(substring, word)
-              >>>True
-    """
-    i = 0
-    result = False
-    while len(word) > i:
-        if word[i] == substring[0]:
-            substring = substring[1:]
-        i += 1
-    print(len(substring) == 0)
+   """
+   This function check if word contains substring
+   
+   @substring: str
+   @word: str
+   @return: bool
+   examples: substring = ''abc''
+             word  = ''ahbgdc''
+             check_if_substring(substring, word)
+             >>>True
+   """
+   i = 0
+   k = 0
+   result = False
+   while len(word) > i:
+       if substring and k < len(substring) and word[i] == substring[k]:
+           k += 1
+       i += 1
+   return k == len(substring)
--- 9_Max_possible_polyndrome.py
+++ 9_Max_possible_polyndrome.py
@@ -16,18 +16,13 @@ def find_max_possible_polyndrome(word: str) -> int:
     result = 0
     for i in dct:
         if dct[i] > 1 and dct[i] % 2 == 1:
-            if dct[i] == dct[max(dct, key=lambda x: dct[x])]:
+            if dct[i] == dct[max(dct, key=lambda x: dct[x] if dct[x] % 2 == 1 else False)] and odd_count == 0:
                 max_odd = dct[i]
-                odd_count = 1     
-            elif dct[max(dct, key=lambda x: dct[x])] % 2 == 0:
-                result += dct[i] 
-                odd_count = 1 
+                odd_count = +1     
             else:
                 result += dct[i] - 1
-                odd_count = 1 
         elif dct[i] % 2 == 0:
             result += dct[i]
     if odd_count == 0 and result != len(word):
         result += 1
     return result + max_odd
-
