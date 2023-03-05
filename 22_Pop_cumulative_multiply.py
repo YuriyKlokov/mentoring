e1c32df019a05eebd5d9e644992714336f51f3e5
YuriyKlokov <60291552+YuriyKlokov@users.noreply.github.com>
2023-03-05 23:41:55 +0300

    Add files via upload

---
+++ 22_Pop_cumulative_multiply.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def pop_cum_multiply(lst: list) -> list:
+    """
+    This function calculate multiplication of list elements for each element without itself
+    @lst: list
+    examples: zero_sequence([5, 1, 4, 2]))
+         >>> [8, 40, 10, 20]
+         # 8 = 1 * 4 * 2
+         # 40 = 5 * 4 * 2
+         # 10 = 5 * 1 * 2
+         # 20 = 5 * 1 * 4
+    """
+    prev_cum_value = 1
+    result = []
+    for i in range(len(lst) - 1):
+        slice_object = slice(i + 1, None)
+        result.append(prev_cum_value * reduce(lambda x, y: x * y, lst[slice_object]))
+        prev_cum_value *= lst[i]
+    else:
+        if len(lst) > 1:
+            result.append(prev_cum_value)
+    return result
+
