62bdee106053acc732bd33034581a1aac5316664
YuriyKlokov <60291552+YuriyKlokov@users.noreply.github.com>
2023-06-28 00:51:21 +0300

    Add files via upload

--- 29_Yandex_funny_numbers.py
+++ 29_Yandex_funny_numbers.py
@@ -1,21 +1,33 @@
-def my_func(number: int, k: int) -> int:
-    """
+def find_max(number, k):
+    '''
     This function crosses out the digit in amount of k. The result is the biggest possible number.
     @number = int
     @k = int
     examples: my_func(5167, 1)
          >>> '567'
-    """
-
+    '''
+    
     int_as_str = list(str(number))
-    result = int_as_str[0]
+    end_point = len(int_as_str) - k 
+    result = [int_as_str[0]]
+    if end_point == 0:
+        return []
     for i in range(1, len(int_as_str)):
-        if len(result) <= len(int_as_str) - k:
+        if len(result) < len(int_as_str) - k: 
             while (
                 len(result) > 0
                 and result[-1] < int_as_str[i]
-                and len(int_as_str) - k - len(result) + 1 <= len(int_as_str) - i
+                and end_point - len(result) + 1 <= len(int_as_str) - i
             ):
-                result = result[:-1]
-        result += int_as_str[i]
-    return int(result)
+                result.pop()
+            result.append(int_as_str[i])
+        else:
+            while (
+                len(result) > 0
+                and result[-1] < int_as_str[i]
+                and end_point - len(result) + 1 <= len(int_as_str) - i
+            ):
+                result.pop()
+                result.append(int_as_str[i])
+    return result
+
